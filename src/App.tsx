69c48bfed4b7e2e1e94c9a6ca8e84165ad11fe38
jeiDev <joseanyeloserrano@gmail.com>
2022-12-09 14:09:39 -0400

    created plane and textures

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+
+import React from 'react'
+
+import { Canvas } from "@react-three/fiber"
+import { Sky } from '@react-three/drei'
+import { Physics } from '@react-three/cannon'
+import { Ground } from './components/Ground'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Canvas>
+        <Sky sunPosition={[100,100, 20]} />
+        <ambientLight intensity={0.5} />
+        <Physics>
+          <Ground />
+        </Physics>
+      </Canvas>
+    </React.Fragment>
   );
 }
 
---
+++ src/components/Ground.tsx
@@ -0,0 +1,16 @@
+import { usePlane } from "@react-three/cannon"
+import { groundTexture } from "../images/textures"
+
+export function Ground(){
+    const [ref] = usePlane<any>(() => ({
+        rotation: [0, 0, 0],
+        position: [0, 0, 0]
+    }))
+    
+    return (
+        <mesh ref={ref}>
+            <planeBufferGeometry attach="geometry" args={[100, 100]}/>
+            <meshStandardMaterial attach="material" map={groundTexture}/>
+        </mesh>
+    )
+}
