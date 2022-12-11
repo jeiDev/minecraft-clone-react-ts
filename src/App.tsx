f1044213b3982b80546b5caf0822a3d343fe8aef
jeiDev <joseanyeloserrano@gmail.com>
2022-12-10 23:41:23 -0400

    created movements

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { Sky } from '@react-three/drei'
 import { Physics } from '@react-three/cannon'
 import { Ground } from './components/Ground'
 import { Player } from './components/Player'
-
+import { FPV } from "./components/FPV"
 
 function App() {
   return (
@@ -15,11 +15,13 @@ function App() {
       <Canvas>
         <Sky sunPosition={[100,100, 20]} />
         <ambientLight intensity={0.5} />
+        <FPV />
         <Physics>
           <Player />
           <Ground />
         </Physics>
       </Canvas>
+      <div className="absolute centered cursor">+</div>
     </React.Fragment>
   );
 }
